bbfd1b3fc8ee1fee0a9bdc130f36f495bdcd6e59
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 12:51:47 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main {
                 System.out.println("          Which emotion resonates?");
                 System.out.println("          Unmotivated, Anxious, Anger, Shame, Sadness?");
                 String action1 = scanner.nextLine();
-             While (action1) {
+                While (action1) {
                     if (action1.equals("Unmotivated") || action1.equals("unmotivated") {
 
                     } else if (action1.equals("Anxious") || action1.equals("anxious") {
@@ -31,14 +31,6 @@ public class Main {
                     } else if (action1.equals("Shame") || action1.equals("shame") {
                     
                     } else if (action1.equals("Sadness") || action1.equals("sadness") {
-                    
-                    } else if (action1.equals("Unmotivated") || action1.equals("unmotivated") {
-
-                    } else if (action1.equals("Anxious") || action1.equals("anxious") {
-                    
-                    } else if (action1.equals("Anger") || action1.equals("anger") {
-                    
-                    } else if (action1.equals("Shame") || action1.equals("shame") {
                     
                     } else if (action1.equals(null) || action1.equals("") {
                         System.out.println("          Please try again!");
