4842e7f9c41d40ea48aab90d4583264e23970dde
tdtle <157906638+tdtle@users.noreply.github.com>
2024-01-27 15:35:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("          How was your day?");
         System.out.println("          Rate your day as 0-4, 5-7, 8-10");
-        System.out.println("          Please write it as '0-4' or '5-7' or '8-10'");
+        System.out.println("          Please write it as "0-4" or "5-7" or "8-10"");
 
         while (scanner.hasNextLine()) {
             String action = scanner.nextLine();
@@ -22,17 +22,15 @@ public class Main {
                 System.out.println("          1.Unmotivated  2.Anxious  3.Anger  4.Shame  5.Sadness? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
-                System.out.println("          'Appearances', 'Finances', 'School', 'Work', 'Family/Relationships', 'Other'.");
+                System.out.println("          Appearances, Finances, School/Work, Family/Relationships, Other.");
                 String action2 = scanner.nextline().toLowerCase();
                 While (action1 && action2) {
                     if (action2.equals("appearances")) {
                         Appearences app = new Appearences(action1);
                     } else if (action2.equals("finances")) {
                         Finances fin = new Finances(action1);;
-                    } else if (action2.equals("school")) {
-                        School sch = new School(action1);
-                    } else if (action2.equals("work")) {
-                        Work wrk = new Work(action1);
+                    } else if (action2.equals("school/work")) {
+                        SchoolWork sch = new SchoolWork(action1);
                     } else if (action2.equals("family/relationships")) {
                         FamilyRship fm = new FamilyRship(action1);
                     } else if (action2.equals("other")) {
@@ -50,17 +48,15 @@ public class Main {
                 System.out.println("          6.Contentment, 7.Boredom, 8.Annoyance, 9.Indifference, 10.Neutrality? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
-                System.out.println("          Appearances, Finances, School, Work, Family/Relationships, Other.");
+                System.out.println("          Appearances, Finances, School/Work, Family/Relationships, Other.");
                 String action2 = scanner.nextline().toLowerCase();
                 While (action1 && action2) {
                     if (action2.equals("appearances")) {
                         Appearences app = new Appearences(action1);
                     } else if (action2.equals("finances")) {
                         Finances fin = new Finances(action1);;
-                    } else if (action2.equals("school")) {
-                        School sch = new School(action1);
-                    } else if (action2.equals("work")) {
-                        Work wrk = new Work(action1);
+                    } else if (action2.equals("school/work")) {
+                        SchoolWork sch = new SchoolWork(action1);
                     } else if (action2.equals("family/relationships")) {
                         FamilyRship fm = new FamilyRship(action1);
                     } else if (action2.equals("other")) {
@@ -77,17 +73,15 @@ public class Main {
                 System.out.println("        11.Joy  12.Gratitude  13.Love  14.Peace  15.Satisfaction? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
-                System.out.println("          Appearances, Finances, School, Work, Family/Relationships, Other.");
+                System.out.println("          Appearances, Finances, School/Work, Family/Relationships, Other.");
                 String action2 = scanner.nextline().toLowerCase();
                 While (action1 && action2) {
                     if (action2.equals("appearances")) {
                         Appearences app = new Appearences(action1);
                     } else if (action2.equals("finances")) {
                         Finances fin = new Finances(action1);;
-                    } else if (action2.equals("school")) {
-                        School sch = new School(action1);
-                    } else if (action2.equals("work")) {
-                        Work wrk = new Work(action1);
+                    } else if (action2.equals("school/work")) {
+                        SchoolWork sch = new SchoolWork(action1);
                     } else if (action2.equals("family/relationships")) {
                         FamilyRship fm = new FamilyRship(action1);
                     } else if (action2.equals("other")) {
@@ -112,3 +106,4 @@ public class Main {
         
     }
 }
+
