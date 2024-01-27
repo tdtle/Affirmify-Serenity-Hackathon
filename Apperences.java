2b599cee09eba6aca8e8f27b0e930ff9208686e8
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 17:19:55 -0500

    Update Apperences.java

--- Apperences.java
+++ Apperences.java
@@ -90,7 +90,7 @@ public class Appearances {
         System.out.println("Log " + i);
         System.out.println(content.get(i-1));
       }
-      System.out.println("Would you like to add any thoughts to your APPERENCES log reflection today?");
+      System.out.println("          Would you like to add any thoughts to your APPERENCES log reflection today?");
     }
   }
 }
