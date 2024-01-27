3ce887cfdd6d49cfa87417473d0211d9a43256a2
adeol20 <157906698+adeol20@users.noreply.github.com>
2024-01-27 12:31:41 -0500

    Update methods.java

--- methods.java
+++ methods.java
@@ -1,3 +1,22 @@
 public class methods {
-    
+    public static void apperences(String str1){
+        System.out.println("affirmation: ");
+    }
+
+    public static void finances(String str1){
+        System.out.println("affirmation: ");
+    }
+
+    public static void schoolWork(String str1){
+        System.out.println("affirmation: ");
+    }
+
+    public static void family(String str1){
+        System.out.println("affirmation: ");
+    }
+
+    public static void other(String str1){
+        System.out.println("affirmation: ");
+    }
 }
+
