24345df31ccecd835814395bfbe0f7b94e70e572
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 17:28:21 -0500

    Update Content.java

--- Content.java
+++ Content.java
@@ -1,13 +1,4 @@
 public class Content
   { 
-    Private String content;
-    public Content(String content) 
-    {
-      this.content = content;
-    } 
-  
-    public print() 
-      { 
-        System.out.println(this.content); 
-      }
-    }
+    public Content(String content) {} 
+  }
