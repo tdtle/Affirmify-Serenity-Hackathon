4f391a68c1b0c9e13f20848b31b2a389847ee18d
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 15:52:00 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,12 +7,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Library myLib = new library();
-        //FINISHHHHHHHHHHH MYLIB
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("          How was your day?");
         System.out.println("          Rate your day as 0-4, 5-7, 8-10");
-        System.out.println("          Please write it as "0-4" or "5-7" or "8-10"");
+        System.out.println("          Please write it as '0-4' or '5-7' or '8-10'");
 
         while (scanner.hasNextLine()) {
             String action = scanner.nextLine();
@@ -22,11 +21,14 @@ public class Main {
                 System.out.println("          1.Unmotivated  2.Anxious  3.Anger  4.Shame  5.Sadness? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
-                System.out.println("          Appearances, Finances, School/Work, Family/Relationships, Other.");
+                System.out.println("          'Appearances', 'Finances', 'School/Work', 'Family/Relationships', 'Other'");
                 String action2 = scanner.nextline().toLowerCase();
                 While (action1 && action2) {
                     if (action2.equals("appearances")) {
                         Appearences app = new Appearences(action1);
+                        //what you learned from the daily affirmationa nd how you think you can achieve 
+                        //it in the future
+                        
                     } else if (action2.equals("finances")) {
                         Finances fin = new Finances(action1);;
                     } else if (action2.equals("school/work")) {
@@ -35,11 +37,14 @@ public class Main {
                         FamilyRship fm = new FamilyRship(action1);
                     } else if (action2.equals("other")) {
                         Other oth = new Other(action1);
-                    }else if (action1.equals(null) || action1.equals("") || action2.equals(null) || action2.equals("")) {
+                    }else {
                         System.out.println("          Please try again!");
                         System.out.println("          Which emotion resonates?");
                         System.out.println("          1.Unmotivated  2.Anxious  3.Anger  4.Shame  5.Sadness? (Do not add spaces or extra characters)");
                         String action1 = scanner.nextLine();
+                        System.out.println("          Why are you feeling this way?");
+                        System.out.println("          'Appearances', 'Finances', 'School/Work', 'Family/Relationships', 'Other'");
+                        String action2 = scanner.nextline().toLowerCase();
                     }
                 }
             
@@ -48,7 +53,7 @@ public class Main {
                 System.out.println("          6.Contentment, 7.Boredom, 8.Annoyance, 9.Indifference, 10.Neutrality? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
-                System.out.println("          Appearances, Finances, School/Work, Family/Relationships, Other.");
+                System.out.println("          'Appearances', 'Finances', 'School/Work', 'Family/Relationships', 'Other'");
                 String action2 = scanner.nextline().toLowerCase();
                 While (action1 && action2) {
                     if (action2.equals("appearances")) {
@@ -61,11 +66,14 @@ public class Main {
                         FamilyRship fm = new FamilyRship(action1);
                     } else if (action2.equals("other")) {
                         Other oth = new Other(action1);
-                    }else if (action1.equals(null) || action1.equals("") || action2.equals(null) || action2.equals("")) {
+                    }else  {
                         System.out.println("          Please try again!");
                         System.out.println("          Which emotion resonates?");
-                        System.out.println("          1.Contentment, 2.Boredom, 3.Annoyance, 4.Indifference, 5.Neutrality? (Do not add spaces or extra characters)");
+                        System.out.println("          6.Contentment, 7.Boredom, 8.Annoyance, 9.Indifference, 10.Neutrality? (Do not add spaces or extra characters)");
                         String action1 = scanner.nextLine();
+                        System.out.println("          Why are you feeling this way?");
+                        System.out.println("          'Appearances', 'Finances', 'School/Work', 'Family/Relationships', 'Other'");
+                        String action2 = scanner.nextline().toLowerCase();
                     }
                 }
             } else if (action.equals("8-10")) {
@@ -73,7 +81,7 @@ public class Main {
                 System.out.println("        11.Joy  12.Gratitude  13.Love  14.Peace  15.Satisfaction? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
-                System.out.println("          Appearances, Finances, School/Work, Family/Relationships, Other.");
+                System.out.println("          'Appearances', 'Finances', 'School/Work', 'Family/Relationships', 'Other'");
                 String action2 = scanner.nextline().toLowerCase();
                 While (action1 && action2) {
                     if (action2.equals("appearances")) {
@@ -86,19 +94,22 @@ public class Main {
                         FamilyRship fm = new FamilyRship(action1);
                     } else if (action2.equals("other")) {
                         Other oth = new Other(action1);
-                    }else if (action1.equals(null) || action1.equals("") || action2.equals(null) || action2.equals("")) {
+                    }else {
                         System.out.println("          Please try again!");
                         System.out.println("          Which emotion resonates?");
-                        System.out.println("        1.Joy  2.Gratitude  3.Love  4.Peace  5.Satisfaction? (Do not add spaces or extra characters)");
+                        System.out.println("        11.Joy  12.Gratitude  13.Love  14.Peace  15.Satisfaction? (Do not add spaces or extra characters)");
                         String action1 = scanner.nextLine();
+                        System.out.println("          Why are you feeling this way?");
+                        System.out.println("          'Appearances', 'Finances', 'School/Work', 'Family/Relationships', 'Other'");
+                        String action2 = scanner.nextline().toLowerCase();
                     }
                 }
 
-            } else if (action == null || action.equals("") {
+            } else {
                 System.out.println("          Please Try Again with the corrrect response!");                
                 System.out.println("          How was your day?");
                 System.out.println("          Rate your day as 0-4, 5-7, 8-10");
-                System.out.println("          Please write it as "0-4" or "5-7" or "8-10"");
+                System.out.println("          Again, please write it as "0-4" or "5-7" or "8-10"");
             }
         }
         
