1684a04c7bc172be150ea66b9c95d457867a3791
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 12:37:55 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,16 +8,22 @@ imprt java.util.*;
 public class Main {
     public static void main(String[] args) {
         Library myLib = new library();
-        
+        //FINISHHHHHHHHHHH MYLIB
         Scanner scanner = new Scanner(System.in);
         System.out.println("          How was your day?");
         System.out.println("          Rate your day as 0-4, 5-7, 8-10");
         System.out.println("          Please write it as "0-4" or "5-7" or "8-10"");
 
         while (scanner.hasNextLine()) {
-            String action = scanner.nextLine;
+            String action = scanner.nextLine();
 
-            if (action.equls("0-4")) {
+            if (action.equals("0-4")) {
+                System.out.println("          Which emotion resonates?");
+                System.out.println("          Unmotivated, Anxious, Anger, Shame, Sadness?");
+                String action1 = scanner.nextLine();
+                if (action1.equals("Unmotivated")) {
+
+                }
             
             } else if (action.equals("5-7")) {
 
