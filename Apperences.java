31d9070bbe6c3183756252c5d289fb501082ba65
adeol20 <157906698+adeol20@users.noreply.github.com>
2024-01-27 15:24:34 -0500

    Update Apperences.java

--- Apperences.java
+++ Apperences.java
@@ -13,46 +13,60 @@ public class Appearances {
         content.add("");
         break;
       case "2":
-        
+        System.out.println();
+        content.add("");
         break;
       case "3":
-        
+        System.out.println();
+        content.add("");
         break;
       case "4":
-        
+        System.out.println();
+        content.add("");
         break;
       case "5":
-        
+        System.out.println();
+        content.add("");
         break 
       case "6":
-        
+        System.out.println();
+        content.add("");
         break;
       case "7":
-        
+        System.out.println();
+        content.add("");
         break;
       case "8":
-        
+        System.out.println();
+        content.add("");
         break;
       case "9":
-        
+        System.out.println();
+        content.add("");
         break;
       case "10":
-        
+        System.out.println();
+        content.add("");
         break;
       case "11":
-        
+        System.out.println();
+        content.add("");
         break;
       case "12":
-        
+        System.out.println();
+        content.add("");
         break;
       case "13":
-        
+        System.out.println();
+        content.add("");
         break;
       case "14":
-        
+        System.out.println();
+        content.add("");
         break;
       case "15":
-        
+        System.out.println();
+        content.add("");
         break 
 
 
