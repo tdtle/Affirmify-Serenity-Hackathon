b8ddc5fd2af2f1b735c3d1287a87ac3251273fc3
tdtle <157906638+tdtle@users.noreply.github.com>
2024-01-27 15:32:02 -0500

    Update Finances.java

--- Finances.java
+++ Finances.java
@@ -8,49 +8,49 @@ public class Finances {
   public Finances(num) {
     switch (num) {
       case "1":
-        System.out.println();
+        System.out.println("I am motivated by the vision of the success that awaits me.");
         break;
       case "2":
-        
+        System.out.println("I choose to focus on what I can control and let go of what I cannot.");
         break;
       case "3":
-        
+        System.out.println("I am resilient in the face of frustration, and I choose a positive response.");
         break;
       case "4":
-        
+        System.out.println("I trust the journey, even when I do not understand it.");
         break;
       case "5":
-        
+        System.out.println(" I am worthy of all the good things life has to offer.");
         break 
       case "6":
-        
+        System.out.println("Every day, I am taking steps toward a more secure financial future.");
         break;
       case "7":
-        
+        System.out.println("Boredom is a signal to explore new activities and experiences, explore new financial oppurtunities. ");
         break;
       case "8":
-        
+        System.out.println("I am grateful for the lessons hidden within moments of irritation.");
         break;
       case "9":
-        
+        System.out.println("I am grateful for the resources I have, and I use them wisely.");
         break;
       case "10":
-        
+        System.out.println("I am grateful for the abundance that flows into my life, and I manage it wisely.");
         break;
       case "11":
-        
+        System.out.println("Joy is the fuel for my soul, and I allow it to light up my life.");
         break;
       case "12":
-        
+        System.out.println("I am grateful for the abundance that surrounds me.");
         break;
       case "13":
-        
+        System.out.println("Love and abundance are my birthrights, and I embrace them with gratitude.");
         break;
       case "14":
-        
+        System.out.println("I let go of the need for control and surrender to the flow of life.");
         break;
       case "15":
-        
+        System.out.println("I release the need for more and embrace the abundance of what I already have.");
         break 
 
 
