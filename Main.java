3a359c6926d74b1d08b571d896bcce586561f8d2
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 15:09:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -47,7 +47,7 @@ public class Main {
             
             } else if (action.equals("5-7")) {
                 System.out.println("          Which emotions resonates?");
-                System.out.println("          1.Contentment, 2.Boredom, 3.Annoyance, 4.Indifference, 5.Neutrality? (Do not add spaces or extra characters)");
+                System.out.println("          6.Contentment, 7.Boredom, 8.Annoyance, 9.Indifference, 10.Neutrality? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
                 System.out.println("          Appearances, Finances, School, Work, Family/Relationships, Other.");
@@ -74,7 +74,7 @@ public class Main {
                 }
             } else if (action.equals("8-10")) {
                 System.out.println("        Which emotions resonate? Please type the number");
-                System.out.println("        1.Joy  2.Gratitude  3.Love  4.Peace  5.Satisfaction? (Do not add spaces or extra characters)");
+                System.out.println("        11.Joy  12.Gratitude  13.Love  14.Peace  15.Satisfaction? (Do not add spaces or extra characters)");
                 String action1 = scanner.nextLine();
                 System.out.println("          Why are you feeling this way?");
                 System.out.println("          Appearances, Finances, School, Work, Family/Relationships, Other.");
