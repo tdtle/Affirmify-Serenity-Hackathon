76522c25c568da2058fafe7209967e4af78d4909
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 15:08:11 -0500

    Update Apperences.java

--- Apperences.java
+++ Apperences.java
@@ -1,9 +1,45 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-
-
 public class Appearances {
+  
+  private ArrayList<Content> content;
+
+  public Appearances(num) {
+    switch (num) {
+      case "1":
+        break;
+      case "2":
+        break;
+      case "3":
+        break;
+      case "4":
+        break;
+      case "5":
+        break 
+      case "6":
+        break;
+      case "7":
+        break;
+      case "8":
+        break;
+      case "9":
+        break;
+      case "10":
+        break;
+      case "11":
+        break;
+      case "12":
+        break;
+      case "13":
+        break;
+      case "14":
+        break;
+      case "15":
+        break 
 
 
+    }
+
+  }
 }
