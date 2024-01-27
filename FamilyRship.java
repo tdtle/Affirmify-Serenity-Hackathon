86a0ebf891580b5fa2dc616488a5f07cadf6c6fb
adeol20 <157906698+adeol20@users.noreply.github.com>
2024-01-27 15:50:29 -0500

    Update FamilyRship.java

--- FamilyRship.java
+++ FamilyRship.java
@@ -9,64 +9,79 @@ public class FamilyRship {
     switch (num) {
       case "1":
         System.out.println("I attract positive and supportive people into my life.");
+        System.out.println("https://www.boundariesbooks.com/ https://www.youtube.com/@watchwellcast https://www.betterhelp.com/");
         content.add("I attract positive and supportive people into my life.");
         break;
       case "2":
         System.out.println(" I am deserving of love, kindness, and understanding.");
+        System.out.println("https://www.boundariesbooks.com/ https://www.youtube.com/@watchwellcast https://www.betterhelp.com/");
         content.add(" I am deserving of love, kindness, and understanding.");
         break;
       case "3":
         System.out.println("I let go of resentment and embrace forgiveness.");
+        System.out.println("https://www.boundariesbooks.com/ https://www.youtube.com/@watchwellcast https://www.betterhelp.com/");
         content.add("I let go of resentment and embrace forgiveness.");
         break;
       case "4":
         System.out.println("My past mistakes do not define my worth or my ability to grow.");
+        System.out.println("https://www.boundariesbooks.com/ https://www.youtube.com/@watchwellcast https://www.betterhelp.com/");
         content.add("My past mistakes do not define my worth or my ability to grow.");
         break;
       case "5":
         System.out.println("I am not defined by my past; I am empowered by my present.");
+        System.out.println("https://www.boundariesbooks.com/ https://www.youtube.com/@watchwellcast https://www.betterhelp.com/");
         content.add("I am not defined by my past; I am empowered by my present.");
         break 
       case "6":
         System.out.println("I actively invest time and effort into my relationships.");
+        System.out.println("https://artonfoundation.bg/en/119.emotional-intelligence-in-love-and-relationships.html https://www.youtube.com/@watchwellcast");
         content.add("I actively invest time and effort into my relationships.");
         break;
       case "7":
         System.out.println("I choose to see my relationships with new eyes, appreciating the person they are becoming. Be proactive in planning activities and surprises.");
+        System.out.println("https://artonfoundation.bg/en/119.emotional-intelligence-in-love-and-relationships.html https://www.youtube.com/@watchwellcast");
         content.add("I choose to see my relationships with new eyes, appreciating the person they are becoming. Be proactive in planning activities and surprises.");
         break;
       case "8":
         System.out.println("I choose to see the bigger picture, minimizing the impact of annoyances.");
+        System.out.println("https://artonfoundation.bg/en/119.emotional-intelligence-in-love-and-relationships.html https://www.youtube.com/@watchwellcast");
         content.add("I choose to see the bigger picture, minimizing the impact of annoyances.");
         break;
       case "9":
         System.out.println("I communicate openly and honestly to strengthen my connections.");
+        System.out.println("https://artonfoundation.bg/en/119.emotional-intelligence-in-love-and-relationships.html https://www.youtube.com/@watchwellcast");
         content.add("I communicate openly and honestly to strengthen my connections.");
         break;
       case "10":
         System.out.println("I am engaged in the dynamics of my relationships, seeking to understand and be understood.");
+        System.out.println("https://artonfoundation.bg/en/119.emotional-intelligence-in-love-and-relationships.html https://www.youtube.com/@watchwellcast");
         content.add("I am engaged in the dynamics of my relationships, seeking to understand and be understood.");
         break;
       case "11":
         System.out.println("My life is filled with moments of pure and unbridled joy.");
+        System.out.println("https://www.health.harvard.edu/staying-healthy/ways-to-keep-your-relationship-strong");
         content.add("My life is filled with moments of pure and unbridled joy.");
         break;
       case "12":
         System.out.println("My heart is filled with gratitude for the love and support I receive.");
+        System.out.println("https://www.health.harvard.edu/staying-healthy/ways-to-keep-your-relationship-strong");
         content.add("My heart is filled with gratitude for the love and support I receive.");
         break;
       case "13":
         System.out.println("My relationships are a reflection of the love I hold within.");
+        System.out.println("https://www.health.harvard.edu/staying-healthy/ways-to-keep-your-relationship-strong");
         content.add("My relationships are a reflection of the love I hold within.");
         break;
       case "14":
         System.out.println("My thoughts are clear, and my mind is a haven of peace.");
+        System.out.println("https://www.health.harvard.edu/staying-healthy/ways-to-keep-your-relationship-strong");
         content.add("My thoughts are clear, and my mind is a haven of peace.");
         break;
       case "15":
         System.out.println("I am content with who I am, where I am, and where I am headed.");
+        System.out.println("https://www.health.harvard.edu/staying-healthy/ways-to-keep-your-relationship-strong");
         content.add("I am content with who I am, where I am, and where I am headed.");
-        break 
+        break; 
 
 
     }
