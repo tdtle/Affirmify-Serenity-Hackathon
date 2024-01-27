d771c57f89295180918664f99ef1f29d4abf14d5
fxtewolde <102703375+fxtewolde@users.noreply.github.com>
2024-01-27 12:29:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,10 +11,21 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("          How was your day?");
-        System.out.println("          Rate your day as 0-3, 4-7, 8
+        System.out.println("          Rate your day as 0-4, 5-7, 8-10");
+        System.out.println("          Please write it as "0-4" or "5-7" or "8-10"");
 
         while (scanner.hasNextLine()) {
-            String action = scanner.nextLine
+            String action = scanner.nextLine;
+
+            if (action.equls("0-4")) {
+            
+            } else if (action.equals("5-7")) {
+
+            } else if (ations.equals("8-10")) {
+
+            } else (action == null || action.equals("") {
+                System.out.print(
+            }
         }
         
         
